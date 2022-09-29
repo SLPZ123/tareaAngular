ce46e3fd6a56775c56820de076d51f0f8d610b21
sofia lopez <sofi.lopez123@outlook.com.ar>
2022-09-29 19:05:08 -0300

    pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular1';
+  constructor(){
+    this.title = 'ListaComponent'
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaComponent } from './component/lista/lista.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { TablaComponent } from './component/tabla/tabla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    NavbarComponent,
+    TablaComponent
   ],
   imports: [
     BrowserModule
